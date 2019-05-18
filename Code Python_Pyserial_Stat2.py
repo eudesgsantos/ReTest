ed77616788120f8c777dd04393b12db9c2c00de6
Eudes Santos <48020875+eudesgsantos@users.noreply.github.com>
2019-05-18 14:02:35 -0300

    Update Code Python_Pyserial_Stat2.py

--- Code Python_Pyserial_Stat2.py
+++ Code Python_Pyserial_Stat2.py
@@ -1,7 +1,7 @@
 import serial
 import time
 import smtplib, ssl
-ArduinoSerial = serial.Serial('COM3')
+ArduinoSerial = serial.Serial('COM16')
 time.sleep(2)
 
 def enviarEmail():
@@ -12,27 +12,23 @@ def enviarEmail():
     port = 465
     password = 'arduinopython'
     sender_email = 'retestping@gmail.com'
-    receiver_email = 'drnb@cesar.school'
+    receiver_email = 'jvca@cesar.school'
 
     message = MIMEMultipart("alternative")
-    message["Subject"] = "FeedBack Ping"
+    message["Subject"] = "multipart test"
     message ["From"] = sender_email
     message ["To"] = receiver_email
     text = """\
     Seu Mario esta indo resolver seu problema,acompanhe seu email para saber mais informações do andamento do problema
-
     """
     html = """\
     <html>
         <body>
             <p>Seu Mario esta indo resolver seu problema, acompanhe seu email para saber mais informações do andamento do problema
-
-
             </p>
         
         </body>
     </html>
-
     """
     part1 = MIMEText(text,"plain")
     part2 = MIMEText(html,"html")
